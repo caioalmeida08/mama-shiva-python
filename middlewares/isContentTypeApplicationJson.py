f3371acbec1291ecfb70a1a853c271eefa6648f0
caioalmeida08 <araujo.caio08@aluno.ifce.edu.br>
2023-09-01 13:02:06 -0300

    fix: metodos GET nao passam por checagem de content-type

--- middlewares/isContentTypeApplicationJson.py
+++ middlewares/isContentTypeApplicationJson.py
@@ -4,7 +4,12 @@ from schemas.mensagem_erro import MensagemErro
 
 class IsContentTypeApplicationJson:
     async def __call__(self, request: Request, call_next):
+        request_method = request.method
+        
         content_type = request.headers.get('Content-Type')
+        
+        if (request_method == "GET"):
+            return await call_next(request)
         
         if (content_type != "application/json"):
             response_status = 400
